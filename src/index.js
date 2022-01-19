5cf97737a42877447d669a651b7a0d0bdaeb7c28
Connor Voelk <voelkc@uw.edu>
2022-01-19 10:24:38 -0800

    adds other stuff?

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { scaleLinear, scaleBand, extent, line, symbol, csv, area } from "d3";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+const data = csv("something", (row) => {
+  // console.log("oof", data);
+  console.log(row);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
