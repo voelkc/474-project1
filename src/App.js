8559e1ee08db37190d3b1de5d171f8f974d08d3c
Connor Voelk <voelkc@uw.edu>
2022-01-20 22:28:47 -0800

    local data,organized with time well

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
 import './index.css'
+import data from './data'
 
 import * as d3 from 'd3'
+
+console.log(data)
+
+// console.log('helo')
+// let aDay = new Date("2014-04-02T07:58:00.000Z")
+// console.log(aDay)
+
+
 
 const month = [
 	'Jan',
@@ -25,66 +34,103 @@ function App() {
 	const margin = 30
 	const legendPadding = 200
 
-	d3.csv(
-		'https://gist.githubusercontent.com/voelkc/8023c515a5289e436f0082d48cad2758/raw/4e460972ba1d629e6c3945962f93ddedae760f18/use-of-force.csv',
-		function (d) {
-			return {
-				id: d.ID,
-				incidentNum: d.Incident_Num,
-				occuredDate: new Date(d.Occured_date_time),
-				precinct: d.Precinct,
-				sector: d.Sector,
-				beat: d.Beat,
-				officerID: d.Officer_ID,
-				subjectID: d.Subject_ID,
-				subjectRace: d.Subject_Race,
-				subjectGender: d.Subject_Gender,
+	// there is a problem when I set, it reruns the function so it just infinitely loops, but I cant access the data in the return otherwise because its outside of the .then
+	//setViz(<p>Loaded</p>)
+
+	//console.log(timeOrganizedData)
+	//download(timeOrganizedData.toString(), 'json.json', 'text/plain');
+
+	let points = [] // made up of objects that have x pos (dependent on the amount in that day), y pos (dependent on day), color, size
+
+	for (const year in data) {
+		//console.log(year)
+		const currentYear = data[year]
+		for (const month in currentYear) {
+			//console.log(month)
+			const currentMonth = currentYear[month]
+			for (const day in currentMonth) {
+				//console.log(day)
+				const currentDay = currentMonth[day]
+				//for (const incident in currentDay) {
+				const incidentPoints = currentDay.map((currentIncident, i) => {
+					let point = {}
+					//console.log(currentIncident)
+					//"2014-04-02T07:58:00.000Z"
+
+					//size
+					if (currentIncident.incidentType.includes('1')) {
+						point.size = 1
+					} else if (currentIncident.incidentType.includes('2')) {
+						point.size = 2
+					} else if (currentIncident.incidentType.includes('3')) {
+						point.size = 3
+					}
+
+					//color
+					if (currentIncident.subjectRace === 'Black or African American') {
+						point.color = 'rgb(255, 0, 0)' // red
+					} else if (currentIncident.subjectRace === 'White') {
+						point.color = 'rgb(0, 255, 0)' // green
+					} else if (currentIncident.subjectRace === 'Asian') {
+						point.color = 'rgb(0, 0, 255)' // blue
+					} else if (currentIncident.subjectRace === 'Hispanic or Latino') {
+						point.color = 'rgb(255, 255, 0)' // yellow?
+					} else if (currentIncident.subjectRace === 'American Indian/Alaska Native') {
+						point.color = 'rgb(255, 0, 255)' // purple?
+					} else if (currentIncident.subjectRace === 'Nat Hawaiian/Oth Pac Islander') {
+						point.color = 'rgb(0, 255, 255)' // teal?
+					} else { // not specified?
+						point.color = 'rgb(200, 200, 255)' // grey
+					}
+
+					//gender
+					if (currentIncident.subjectGender === 'Male') {
+						point.gender = 'male'
+					} else if (currentIncident.subjectGender === 'Female') {
+						point.gender = 'female'
+					} else { // not specified
+						point.gender = 'none'
+					}
+
+					// incident position (height!)
+					point.yPosition = i
+
+					point.date = new Date(currentIncident.occuredDate)
+
+					//point.xPosition = currentIncident.occuredDate.getFullYear() % 4 == 0 ? 366 : 365;
+					//"2014-04-02T07:58:00.000Z"
+					let day = Date.parse(currentIncident.occuredDate)
+					//console.log(typeof day)
+					// console.log(currentIncident.occuredDate)
+					// console.log((day / 10000) - 140166378)
+					//var days = new Date().getFullYear() % 4 == 0 ? 366 : 365;
+
+					return point
+				})
+				console.log(year, month, day, incidentPoints)
 			}
 		}
-	).then(function (data) {
-		// put the viz here?
-		//aggregate the months/years
-		let timeOrganizedData = {}
-		data.forEach((incident) => {
-			const incYear = incident.occuredDate.getFullYear()
-			const incMonth = month[incident.occuredDate.getMonth()]
-			const incDay = incident.occuredDate.getDate()
-			//console.log(incYear, incMonth, incDay);
+	}
 
-			if (incYear in timeOrganizedData) {
-				if (incMonth in timeOrganizedData[incYear]) {
-					if (incDay in timeOrganizedData[incYear][incMonth]) {
-						timeOrganizedData[incYear][incMonth][incDay].push(incident) // add this incident to the list for this month
-					} else {
-						timeOrganizedData[incYear][incMonth][incDay] = [incident] // adds a new list with just this incident
-					}
-				} else {
-					timeOrganizedData[incYear][incMonth] = {}
-					timeOrganizedData[incYear][incMonth][incDay] = [incident] // adds a new list with just this incident
-				}
-			} else {
-				timeOrganizedData[incYear] = {}
-				timeOrganizedData[incYear][incMonth] = {}
-				timeOrganizedData[incYear][incMonth][incDay] = [incident] // adds a new list with just this incident
-			}
-		})
-		//setViz(<p>Loaded</p>)
-		console.log(timeOrganizedData)
 
-		// loop through all of the months
-		// for (const year in timeOrganizedData) {
-		//   const setYear = timeOrganizedData[year];
-		//   for (const month in setYear) {
-		//     console.log(year, month, setYear[month].length);
-		//   }
-		// }
-	})
+	// loop through all of the months
+	// for (const year in timeOrganizedData) {
+	//   const setYear = timeOrganizedData[year];
+	//   for (const month in setYear) {
+	//     console.log(year, month, setYear[month].length);
+	//   }
+	// }
 
 	return (
 		<div>
 			<h1>Seattle Police Department Use-of-Force Incidents</h1>
-			{viz}
-		</div>
+			<svg
+				width={chartSize * 2}
+				height={chartSize}
+				style={{ border: "2px solid white" }}>
+
+			</svg>
+		</div >
 	)
 }
 
